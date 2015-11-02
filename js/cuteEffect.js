17b9f7691a3165fc2acbb82ce642e894e7167624
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2015-11-02 17:27:22 +0000

    Website has image. Changes color.
    Text still missing. Needs form formatting.
    
    Needs Google Analytics.

---
+++ js/cuteEffect.js
@@ -0,0 +1,15 @@
+var POLL_PERIOD = 1000; // in milliseconds
+
+$( document ).ready(function() {
+	var data = 5;
+	setInterval(function () {cuteEffect();}, POLL_PERIOD);
+
+	function cuteEffect() {
+		if(data % 10 > 5) {
+			$("#drawing").css("background-color", "red");
+		} else {
+			$("#drawing").css("background-color", "limegreen");
+		}
+		data++;
+	}
+});
